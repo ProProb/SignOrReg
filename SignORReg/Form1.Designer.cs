1a7e871515af908a627bde63ae4a21faf73c9f66
vin.roma.spb <vin.roma.spb@gmail.com>
2019-08-16 17:28:32 +0300

    start

--- SignORReg/Form1.Designer.cs
+++ SignORReg/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sign = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sign
+            // 
+            this.sign.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.sign.Location = new System.Drawing.Point(460, 277);
+            this.sign.Name = "sign";
+            this.sign.Size = new System.Drawing.Size(101, 23);
+            this.sign.TabIndex = 0;
+            this.sign.Text = "Sign in";
+            this.sign.UseVisualStyleBackColor = false;
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.close.Location = new System.Drawing.Point(329, 277);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 23);
+            this.close.TabIndex = 1;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(460, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.Location = new System.Drawing.Point(329, 214);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(33, 13);
+            this.login.TabIndex = 4;
+            this.login.Text = "Login";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Location = new System.Drawing.Point(460, 214);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(53, 13);
+            this.pass.TabIndex = 5;
+            this.pass.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sign up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.sign);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sign;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label login;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SignORReg/Form2.cs
+++ SignORReg/Form2.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SignORReg
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
@@ -24,8 +24,13 @@ namespace SignORReg
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
+            if (checkBox1.Checked == true)
+            {
+                Form3 form3 = new Form3();
+                form3.Show();
+            }
+            else
+                MessageBox.Show("You need to agree with our rules");
         }
     }
 }
