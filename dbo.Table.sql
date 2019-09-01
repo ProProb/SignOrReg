fa701b04576a7bc74ee0668c46036934952945dd
vin.roma.spb <vin.roma.spb@gmail.com>
2019-09-01 19:22:23 +0300

    n

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Phone number] NCHAR(10) NULL, 
+    [Login] NCHAR(10) NULL, 
+    [Password] NCHAR(10) NULL, 
+    [Name] NCHAR(10)  NULL
+)
